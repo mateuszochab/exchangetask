aecac944c2d2d0c3506536be7e5fff4dbbe5f618
ochab.mat@gmail.com <ochab.mat@gmail.com>
2024-11-04 14:20:46 +0100

    initial commit

---
+++ src/main/java/com/mateuszochab/exchangetask/ExchangetaskApplication.java
@@ -0,0 +1,13 @@
+package com.mateuszochab.exchangetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangetaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangetaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mateuszochab/exchangetask/ExchangetaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuszochab.exchangetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangetaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
